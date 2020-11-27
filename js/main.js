27afd0163635b986244fd016119ed33bae5aed10
Daniel Gonzalez <cadagoro@gmail.com>
2020-11-27 14:32:07 -0300

    Version 1.5: Se agrego la seccion de Teams Member en el archivo de HTML y CSS con su corresponientes Media Query para el responsive/ tambien se agrego las librerias js y css para el Carousel con los script en el archivo main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //    SCROLL NAVBAR
     $(window).scroll(function () {
         // sticky navbar on scroll script
         if (this.scrollY > 20) {
@@ -13,5 +14,27 @@ $(document).ready(function () {
     $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
+
+    // OWL CAROUSEL SCRIPT
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
     });
 });
